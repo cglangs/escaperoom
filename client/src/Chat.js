bb66851268050d268181d9f732d22549b5c69d0b
cglangs <cdanegallagher@gmail.com>
2020-12-08 19:02:10 -0400

    add static

---
+++ client/src/Chat.js
@@ -0,0 +1,20 @@
+/*import * as GUI from '@babylonjs/gui'
+
+
+export default class Chat {
+	static init(){
+		this.advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("ChatUI");
+        var inputBox =  new GUI.InputText()
+        inputBox.width = 0.2;
+        inputBox.maxWidth = 0.2;
+        inputBox.height = '40px';
+        inputBox.color = "white";
+        inputBox.text = "hello";
+        inputBox.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        inputBox.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        this.advancedTexture.addControl(inputBox);
+	}
+
+
+
+}*/
